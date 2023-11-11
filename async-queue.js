f67c6f2062f65768d60138d050fe745822a57a39
Breno Almeida <brenimsilva7@gmail.com>
2023-11-11 16:23:58 -0300

    [Feature]: Front

--- async-queue.js
+++ async-queue.js
@@ -8,62 +8,117 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const elements = {
+    btnExecuteQueue: document.getElementById("btn-queue-submit"),
+    btnNextTask: document.getElementById("btn-queue-next"),
+    taskMessagesContainer: document.getElementById("task-messages"),
+    taskNextContainer: document.getElementById("task-container-next"),
+    taskContainer: document.getElementById("task-container-queue"),
+    btnSubmit: document.getElementById("btn-submit"),
+    taskTextInput: document.getElementById("inp-task-text"),
+    taskDelayInput: document.getElementById("inp-task-delay"),
+    taskNameInput: document.getElementById("inp-task-name"),
+    timer: document.querySelector(".inner-timer"),
+};
 class AsyncQueue {
     constructor() {
         this.queue = [];
-        this.isProcessing = false;
+        this._isProcessing = false;
     }
-    enqueue(task, taskName) {
+    enqueue() {
+        const taskElement = this.createTask();
         return new Promise((resolve, reject) => {
-            this.queue.push({ task, resolve, reject, taskName });
+            this.queue.push({
+                resolve,
+                reject,
+                task: taskElement.cb,
+                taskName: taskElement.taskName,
+                delay: taskElement.delay,
+            });
+            this._refreshQueue();
         });
+    }
+    _refreshQueue() {
+        elements.taskContainer.innerHTML = "";
+        this.queue.forEach((t) => {
+            const p = document.createElement("p");
+            p.innerText = `${t.taskName}()`;
+            elements.taskContainer.appendChild(p);
+        });
+    }
+    dequeue() {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (this.queue.length <= 0)
+                return console.error("Sem Tasks para remover da fila");
+            const { task, resolve, reject, taskName, delay } = this.queue.shift();
+            this._refreshQueue();
+            const p = document.createElement("p");
+            p.innerText = `${taskName}()`;
+            elements.taskNextContainer.appendChild(p);
+            this._fillDelayBar(delay);
+            try {
+                const result = yield task();
+                elements.taskMessagesContainer.innerText = result;
+                resolve(result);
+                elements.taskNextContainer.innerHTML = "";
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    _fillDelayBar(delay) {
+        let fill = 0;
+        const interval = setInterval(() => {
+            fill += ((1 / delay) * 1);
+            elements.timer.style.width = `${fill}%`;
+        }, 10);
+        setTimeout(() => {
+            clearInterval(interval);
+            elements.timer.style.width = `0%`;
+        }, delay * 1000);
+    }
+    createTask() {
+        const taskName = elements.taskNameInput.value;
+        const taskText = elements.taskTextInput.value;
+        const taskDelay = Number(elements.taskDelayInput.value);
+        elements.taskNameInput.value = "";
+        elements.taskTextInput.value = "";
+        elements.taskDelayInput.value = '';
+        return {
+            taskName: taskName,
+            delay: taskDelay,
+            cb: () => {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve(taskText);
+                    }, taskDelay * 1000);
+                });
+            }
+        };
     }
     processQueue() {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.isProcessing)
+            if (this._isProcessing)
                 return;
-            this.isProcessing = true;
+            this._isProcessing = true;
             while (this.queue.length > 0) {
-                console.log(this.queue);
-                const { task, resolve, reject } = this.queue.shift();
-                try {
-                    console.log("resolvendo task");
-                    const result = yield task();
-                    resolve(result);
-                }
-                catch (error) {
-                    reject(error);
-                }
+                yield this.dequeue();
             }
-            this.isProcessing = false;
+            this._isProcessing = false;
         });
     }
 }
 const q = new AsyncQueue();
-q.enqueue(t1, "Task 1").then(resolve => console.log(resolve));
-q.enqueue(t2, "Task 2").then(resolve => console.log(resolve));
-q.enqueue(t3, "Task 3").then(resolve => console.log(resolve));
-function t1() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Timeout T1");
-            resolve("Resultado do resolve da T1");
-        }, 1000);
-    });
-}
-function t2() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Timeout T2");
-            resolve("Resultado do resolve da T2");
-        }, 1000);
-    });
-}
-function t3() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Timeout T3");
-            resolve("Resultado do resolve da T3");
-        }, 1000);
-    });
-}
+elements.btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    q.enqueue();
+});
+elements.btnNextTask.addEventListener("click", (e) => __awaiter(void 0, void 0, void 0, function* () {
+    e.preventDefault();
+    const res = q.dequeue();
+}));
+elements.btnExecuteQueue.addEventListener("click", (e) => {
+    e.preventDefault();
+    q.processQueue();
+});
--- async-queue.ts
+++ async-queue.ts
@@ -1,74 +1,135 @@
+const elements = {
+    btnExecuteQueue: document.getElementById("btn-queue-submit") as HTMLButtonElement,
+    btnNextTask: document.getElementById("btn-queue-next") as HTMLButtonElement,
+    taskMessagesContainer: document.getElementById("task-messages") as HTMLDivElement,
+    taskNextContainer: document.getElementById("task-container-next") as HTMLDivElement,
+    taskContainer: document.getElementById("task-container-queue") as HTMLDivElement,
+    btnSubmit: document.getElementById("btn-submit") as HTMLButtonElement,
+    taskTextInput: document.getElementById("inp-task-text") as HTMLInputElement,
+    taskDelayInput: document.getElementById("inp-task-delay") as HTMLInputElement,
+    taskNameInput: document.getElementById("inp-task-name") as HTMLInputElement,
+    timer: document.querySelector(".inner-timer") as HTMLDivElement,
+}
+
+
 type Task = {
     task: () => Promise<any>
+    delay: number;
     resolve: (value: any) => void;
     reject: (value: any) => void;
     taskName: string;
 }
+
 class AsyncQueue {
-    private queue: Task[]
-    private isProcessing: boolean 
+    public queue: Task[]
+    private _isProcessing: boolean 
     constructor(){
         this.queue = [];
-        this.isProcessing = false;
+        this._isProcessing = false;
     }
 
-    enqueue(task: () => Promise<any>, taskName: string) {
+    enqueue() {
+        const taskElement = this.createTask()
         return new Promise((resolve,reject) => {
-            this.queue.push({task,resolve,reject, taskName});
+            this.queue.push({
+                resolve,
+                reject,
+                task: taskElement.cb,
+                taskName: taskElement.taskName,
+                delay: taskElement.delay,
+            });
+            this._refreshQueue();
         })
     }
 
-    async processQueue() {
-        if(this.isProcessing) return;
+    private _refreshQueue() {
+        elements.taskContainer.innerHTML = "";
+        this.queue.forEach((t) => {
+            const p = document.createElement("p");
+            p.innerText = `${t.taskName}()`
+            elements.taskContainer.appendChild(p);
+        })
+    }
 
-        this.isProcessing = true;
+    async dequeue() {
+        if(this.queue.length <= 0) return console.error("Sem Tasks para remover da fila");
+        const {task,resolve,reject,taskName, delay} = this.queue.shift()!;
+        this._refreshQueue();
+        const p = document.createElement("p");
+        p.innerText = `${taskName}()`;
+        elements.taskNextContainer.appendChild(p);
+        this._fillDelayBar(delay);
+        try {
+            const result = await task();
+            elements.taskMessagesContainer.innerText = result;
+            resolve(result)
+            elements.taskNextContainer.innerHTML = ""
+        }
+        catch(error) {
+            reject(error);
+        }
+    }
 
-        while(this.queue.length > 0) {
-            console.log(this.queue);
-            const {task,resolve,reject} = this.queue.shift()!;
-            try{
-                console.log("resolvendo task")
-                const result = await task();
-               resolve(result);
-            }
-            catch(error) {
-                reject(error);
+    private _fillDelayBar(delay: number) {
+        let fill = 0;
+        const interval = setInterval(() => {
+            fill += ((1/delay) * 1);
+            elements.timer.style.width = `${fill}%`
+        }, 10)
+        setTimeout(() => {
+            clearInterval(interval);
+            elements.timer.style.width = `0%`;
+        }, delay * 1000)
+    }
+
+    createTask() : {delay: number,taskName: string, cb: () => Promise<any>} {
+        const taskName = elements.taskNameInput.value;
+        const taskText = elements.taskTextInput.value;
+        const taskDelay = Number(elements.taskDelayInput.value);
+        elements.taskNameInput.value = ""
+        elements.taskTextInput.value = ""
+        elements.taskDelayInput.value = ''
+        return {
+            taskName: taskName,
+            delay: taskDelay,
+            cb: () => {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve(taskText)
+                    },taskDelay * 1000)
+                })
             }
         }
 
-        this.isProcessing = false;
+    }
+
+    async processQueue() {
+        if(this._isProcessing) return;
+
+        this._isProcessing = true;
+
+        while(this.queue.length > 0) {
+            await this.dequeue();
+        }
+
+        this._isProcessing = false;
     }
 }
 
 const q = new AsyncQueue();
 
-q.enqueue(t1, "Task 1").then(resolve => console.log(resolve));
-q.enqueue(t2, "Task 2").then(resolve => console.log(resolve));
-q.enqueue(t3, "Task 3").then(resolve => console.log(resolve));
+elements.btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    q.enqueue();
+})
+
+elements.btnNextTask.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const res = q.dequeue();
+})
 
 
-
-function t1() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Timeout T1")
-            resolve("Resultado do resolve da T1")
-        },1000)
-    })
-}
-function t2() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Timeout T2")
-            resolve("Resultado do resolve da T2")
-        },1000)
-    })
-}
-function t3() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Timeout T3")
-            resolve("Resultado do resolve da T3")
-        },1000)
-    })
-}
+elements.btnExecuteQueue.addEventListener("click", (e) => {
+    e.preventDefault();
+    q.processQueue();
+})
